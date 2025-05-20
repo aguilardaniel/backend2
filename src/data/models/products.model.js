a25d6ad72da95084080fa728b253dd394023f06d
aguilardaniel <agdaniel89@gmail.com>
2025-05-20 13:46:21 -0300

    passport google

--- src/data/models/products.model.js
+++ src/data/models/products.model.js
@@ -5,7 +5,10 @@ const schema = new Schema(
   {
     title: { type: String, required: true, index: true },
     description: { type: String },
-    category: { type: String, default: "Laptops", enum: ["Tablets", "Smartphones", "Laptops", "Smartwatches", "Headphones", "Speakers", "Desktops", "Streaming Devices", "Keyboards", "Accessories", "Virtual Reality", "Fitness", "Cameras", "Gaming", "Televisions", "Soundbars"], index: true },
+    
+    category: { type: String, default: "Aromaterapia", index: true },
+    //category: { type: String, default: "Laptops", enum: ["Tablets", "Smartphones", "Laptops", "Smartwatches", "Headphones", "Speakers", "Desktops", "Streaming Devices", "Keyboards", "Accessories", "Virtual Reality", "Fitness", "Cameras", "Gaming", "Televisions", "Soundbars"], index: true },
+    
     image: { type: String, default: "https://www.shutterstock.com/image-vector/missing-picture-page-website-design-600nw-1552421075.jpg" },
     price: { type: Number, default: 10 },
     stock: { type: Number, default: 10 },
